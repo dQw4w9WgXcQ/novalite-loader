aecc912fbeb8e7a1abf451a62add51ad4b883032
dQw4w9WgXcQ <22358065+dQw4w9WgXcQ@users.noreply.github.com>
2023-04-27 13:59:09 -0700

    loggin in works

--- src/main/java/io/novalite/auth/Auth.java
+++ src/main/java/io/novalite/auth/Auth.java
@@ -53,7 +53,7 @@ public class Auth {
 
                 req.getResponseHeaders().set("Content-Type", "text/html; charset=utf-8");
                 req.sendResponseHeaders(200, 0);
-                req.getResponseBody().write("loggin in... you can close this page.  If you do not get logged in, check the logger for errors.".getBytes(StandardCharsets.UTF_8));
+                req.getResponseBody().write("You can close this page.  If you do not get logged in, check the logger for errors.".getBytes(StandardCharsets.UTF_8));
             } catch (Exception e) {
                 log.warn("failure serving oauth response", e);
                 req.sendResponseHeaders(400, 0);
